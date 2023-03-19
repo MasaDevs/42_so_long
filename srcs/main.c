9a3fd1c453d927d8a1adf46f8dab171337135bea
MasaDevs <110250805+MasaDevs@users.noreply.github.com>
2023-03-19 17:34:01 +0900

    add pic

--- srcs/main.c
+++ srcs/main.c
@@ -1,12 +1,11 @@
 #include "so_long.h"
 
-typedef struct	s_data {
+typedef struct	s_image {
 	void	*img;
-	char	*addr;
-	int		bits_per_pixel;
-	int		line_length;
-	int		endian;
-}				t_data;
+	char	*relative_path;
+	int		img_width;
+	int		img_height;
+}				t_image;
 
 typedef struct s_vars
 {
@@ -14,13 +13,6 @@ typedef struct s_vars
 	void	*win;
 }				t_vars;
 
-void	my_mlx_pixel_put(t_data *data, int x, int y, int color)
-{
-	char	*dst;
-
-	dst = data->addr + (y * data->line_length + x * (data->bits_per_pixel / 8));
-	*(unsigned int*)dst = color;
-}
 
 int close(int keycode, t_vars *vars)
 {
@@ -37,21 +29,15 @@ int close(int keycode, t_vars *vars)
 int	main(void)
 {
 	t_vars	vars;
-	t_data	img;
+	t_image	img;
 
 	vars.mlx = mlx_init();
 	vars.win= mlx_new_window(vars.mlx, 1080, 1080, "Hello world!");
-	img.img = mlx_new_image(vars.mlx, 50, 1080);
-	img.addr = mlx_get_data_addr(img.img, &img.bits_per_pixel, &img.line_length,
-								&img.endian);
-	for (size_t j = 0; j < 1080; j++)
-	{
-		for (size_t i = 0; i < 1080; i++)
-		{
-			my_mlx_pixel_put(&img, j, i, 0x00FFFF);
-		}
-	}
-	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 0, 0);
+	img.img = mlx_xpm_file_to_image(vars.mlx, "./images/nami.xpm", &img.img_width, &img.img_height);
+	if(img.img == NULL)
+		printf("no image\n");
+	printf("%d %d\n", img.img_height, img.img_width);
+	mlx_put_image_to_window(vars.mlx, vars.win, img.img, 110, 110);
 	mlx_hook(vars.win, 2, 1L<<0, close, &vars);
 	mlx_loop(vars.mlx);
 }
